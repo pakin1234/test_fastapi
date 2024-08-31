a0901a7de1535d97beca816b9c20e0b4a089dca7
pakin123 <pavelfoshkin1@icloud.com>
2024-08-31 11:55:27 +0300

    updated lifespanevents

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
 
+from contextlib import asynccontextmanager
+import uvicorn
+
 from api.endpoints.product import product_router
 from api.endpoints.category import category_router
 
 from database import database
 
-app = FastAPI()
-
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     await database.connect()
+    try:
+      yield
+    finally:
+      await database.disconnect
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+
+app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(product_router, prefix="/products")
@@ -23,3 +27,6 @@ app.include_router(category_router, prefix="/categories")
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
